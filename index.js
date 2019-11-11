8940513e55f79c6320505907b59e10d0083cfacb
butexx <47323729+butexx@users.noreply.github.com>
2019-11-11 21:06:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let options = {
         username: 'Butexx',
         password: process.env.B0T_T0KEN,
     },
-    channels: ['Nieuczesana', 'sodapoppin', 'shroud', 'FederalGhosts', 'C9Sneaky', 'STPeach', 'Brunecia', 'Miauki', 'AiSzjaa', 'ikusiaa', 'Amouranth', 'forsen', 'xQcOW', 'anieyo_', 'an_dream', 'AnniTheDuck', 'Denly', 'MisDelish', 'suvikh', 'yuki2yuki', '2sick4drama', 'kachynska', 'DansGaming', 'RajjPatel', 'AriGameplays', 'Jenna', 'RomanovaRela', 'Emzia', 'SteamingPolak', 'TheSwatKnight', 'Stazjaa', 'H2P_Gucio', 'demonzz1', 'miamalkova', 'modestal', 'pqueen', 'Bnans', 'shuteye_orange', 'kaceytron', 'KBubblez', 'Viktorkaa', 'MSFIIIRE', 'BrandonGrych', 'MissCoookiez', 'GiannieLee'],
+    channels: ['Nieuczesana', 'sodapoppin', 'shroud', 'FederalGhosts', 'C9Sneaky', 'STPeach', 'Brunecia', 'Miauki', 'AiSzjaa', 'ikusiaa', 'Amouranth', 'forsen', 'xQcOW', 'anieyo_', 'an_dream', 'AnniTheDuck', 'Denly', 'MisDelish', 'suvikh', 'yuki2yuki', '2sick4drama', 'kachynska', 'DansGaming', 'RajjPatel', 'AriGameplays', 'Jenna', 'RomanovaRela', 'Emzia', 'SteamingPolak', 'TheSwatKnight', 'Stazjaa', 'H2P_Gucio', 'demonzz1', 'miamalkova', 'modestal', 'pqueen', 'Bnans', 'shuteye_orange', 'kaceytron', 'KBubblez', 'Viktorkaa', 'MSFIIIRE', 'BrandonGrych', 'MissCoookiez', 'GiannieLee', 'Sweet_Anita'],
 };
 
 let client = new tmi.client(options);
